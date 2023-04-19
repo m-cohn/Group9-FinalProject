715d704a43ad2e3d300202e2600337c7908f8dde
unknown <mcohn@vt.edu>
2023-04-19 19:45:11 -0400

    Log regression somewhat finished

--- logregression.py
+++ logregression.py
@@ -1,6 +1,5 @@
 from sklearn.linear_model import LogisticRegression
-from joblib import dump
 
 def main(data, labels):    
-    model = LogisticRegression().fit(data, labels)
-    dump(model, "model/model.joblib")
+    model = LogisticRegression(max_iter=5000).fit(data, labels)
+    return model
